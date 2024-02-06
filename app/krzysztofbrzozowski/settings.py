f29f10539b579b494564138d9b9949820d6c677f
Krzysztof Brzozowski <krzysztof_brzozowski@icloud.com>
2024-02-06 23:47:32 +0100

    Debug flag has other implications - need to find where

--- app/krzysztofbrzozowski/settings.py
+++ app/krzysztofbrzozowski/settings.py
@@ -206,7 +206,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_in_env')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-print(f'output from settings -> {STATICFILES_DIRS: }, {STATIC_ROOT: }, {MEDIA_ROOT: }')
+print(f'output from settings -> {STATICFILES_DIRS=}, {STATIC_ROOT=}, {MEDIA_ROOT=}')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
